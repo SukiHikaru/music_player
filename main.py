f7f58be30e8d2c08eb33d4122434847629587a8e
suki <suki.nguyen@aco.charity>
2020-03-18 20:03:34 +0100

    add pause btn

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 from tkinter import *
+import tkinter as TK
+from pygame import mixer    # mixer is responsible for playing music
 
 root = Tk()
+mixer.init()    # initializing
+root.geometry('300x300')
 root.title('Melody')
+root.iconbitmap(r'melody.ico')
+
+text = TK.Label(root, text='Lets make some noise!')
+text.pack()
 
 
 
 
+def playMusic():
+    # load the file
+    mixer.music.load('journey.wav')
+    mixer.music.play()
+
+def stopMusic():
+    mixer.music.stop()
 
 
+playPhoto = TK.PhotoImage(file='music-player.png')
+playBtn = TK.Button(root, image=playPhoto, command = playMusic)
+playBtn.pack()
+stopPhoto = TK.PhotoImage(file='stop.png')
+stopBtn =TK.Button(root, image=stopPhoto, command=stopMusic)
+stopBtn.pack()
 
 
 
